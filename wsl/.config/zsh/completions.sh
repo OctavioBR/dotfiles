31907bbcdfdfe2c347c42c9bce6a2dd9d05af039
Octávio Richter <4429577+OctavioBR@users.noreply.github.com>
2024-12-17 20:13:42 -0300

    Fix completions

--- wsl/.config/zsh/completions.sh
+++ wsl/.config/zsh/completions.sh
@@ -1,10 +1,10 @@
 # FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 # Kubectl
-# (( $+commands[kubectl] )) && source <(kubectl completion zsh) # FIXME: command not found: kubectl
+(( $+commands[kubectl] )) && source <(kubectl completion zsh)
 # NPM completion
 (( $+commands[npm] )) && source <(npm completion)
 # Helm completion
-# (( $+commands[helm] )) && source <(helm completion zsh) # FIXME: Kubernetes configuration file is group-readable. This is insecure. Location: /home/octavio/.kube/config
+(( $+commands[helm] )) && source <(helm completion zsh 2>/dev/null) # FIXME: Kubernetes configuration file is group-readable. This is insecure. Location: /home/octavio/.kube/config
 # Pack CLI
-# (( $+commands[pack] )) && source <(pack completion --shell zsh)
+(( $+commands[pack] )) && source "$(pack completion --shell zsh)"
